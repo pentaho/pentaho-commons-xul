7ed712daba05b3bbcf5cb5f795874a45e96611c5
Aaron Phillips <aphillips@pentaho.com>
2008-05-05 01:54:25 +0000

    fixed layouting problem with swing radiogroup

--- src/org/pentaho/ui/xul/swing/tags/SwingRadioGroup.java
+++ src/org/pentaho/ui/xul/swing/tags/SwingRadioGroup.java
@@ -3,16 +3,17 @@
  */
 package org.pentaho.ui.xul.swing.tags;
 
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulDomContainer;
+import org.pentaho.ui.xul.XulDomException;
 import org.pentaho.ui.xul.components.XulRadioGroup;
 import org.pentaho.ui.xul.swing.SwingElement;
 import org.pentaho.ui.xul.util.Orient;
@@ -22,37 +23,60 @@ import org.pentaho.ui.xul.util.Orient;
  *
  */
 public class SwingRadioGroup extends SwingElement implements XulRadioGroup {
-	private static final Log logger = LogFactory.getLog(SwingRadioGroup.class);
+
 	private ButtonGroup buttonGroup = new ButtonGroup();
 
-  public SwingRadioGroup(XulComponent parent, XulDomContainer domContainer, String tagName) {
-    super("radiogroup");
-    container = new JPanel(new GridBagLayout());
-    
-    managedObject = container;
-    
-    resetContainer();
-  }
+	public SwingRadioGroup(XulComponent parent, XulDomContainer domContainer,
+			String tagName) {
+		super("radiogroup");
 
-  @Override
-  public void addComponent(XulComponent c) {
-    addComponentToButtonGroup(c);
+		this.orientation = Orient.VERTICAL;
 
-    super.addComponent(c);
-  }
-  
-  protected void addComponentToButtonGroup(XulComponent c) {
-    for(XulComponent child : c.getChildNodes()) {
-      addComponentToButtonGroup(child);
-    }
-    if(AbstractButton.class.isAssignableFrom(c.getManagedObject().getClass())) {
-      this.buttonGroup.add((AbstractButton)c.getManagedObject());
-    }
-  }
-  
-  @Override
-  public Orient getOrientation() {
-    return Orient.VERTICAL;
-  }
+		container = new JPanel(new GridBagLayout());
+
+		managedObject = container;
+
+		resetContainer();
+	}
+
+	@Override
+	public void addComponent(XulComponent c) {
+		addComponentToButtonGroup(c);
+
+		super.addComponent(c);
+	}
+
+	protected void addComponentToButtonGroup(XulComponent c) {
+		for (XulComponent child : c.getChildNodes()) {
+			addComponentToButtonGroup(child);
+		}
+		if (AbstractButton.class.isAssignableFrom(c.getManagedObject()
+				.getClass())) {
+			this.buttonGroup.add((AbstractButton) c.getManagedObject());
+		}
+	}
+
+	public void resetContainer() {
+
+		container.removeAll();
+
+		gc = new GridBagConstraints();
+		gc.gridy = GridBagConstraints.RELATIVE;
+		gc.gridx = 0;
+		gc.gridheight = 1;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.insets = new Insets(2, 2, 2, 2);
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		gc.anchor = GridBagConstraints.NORTHWEST;
+		gc.weightx = 1;
+
+	}
+
+	@Override
+	public void replaceChild(XulComponent oldElement, XulComponent newElement)
+			throws XulDomException {
+		this.resetContainer();
+		super.replaceChild(oldElement, newElement);
+	}
 }  
   
