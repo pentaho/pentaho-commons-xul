f060f3686843196b651d4db7a5a1d8eb108dbe0b
Aaron Phillips <aphillips@pentaho.com>
2008-05-25 16:55:11 +0000

    cleanup imports

--- src/org/pentaho/ui/xul/swing/tags/SwingButton.java
+++ src/org/pentaho/ui/xul/swing/tags/SwingButton.java
@@ -3,17 +3,13 @@
  */
 package org.pentaho.ui.xul.swing.tags;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.net.URL;
 
 import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
