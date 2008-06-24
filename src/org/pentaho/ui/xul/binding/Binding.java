1bd08d38969137e24999f808cf7ad69082e12852
Aaron Phillips <aphillips@pentaho.com>
2008-06-24 15:57:31 +0000

    reworked Bindings so Binding class is standalone aka it's the only thing you need to actually create a binding

--- src/org/pentaho/ui/xul/binding/Binding.java
+++ src/org/pentaho/ui/xul/binding/Binding.java
@@ -1,6 +1,15 @@
 package org.pentaho.ui.xul.binding;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Stack;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.pentaho.ui.xul.XulDomContainer;
+import org.pentaho.ui.xul.XulEventSource;
 import org.pentaho.ui.xul.binding.BindingConvertor.Direction;
 
 public class Binding {
@@ -14,10 +23,18 @@ public class Binding {
   private String targetAttr;
 
   private boolean negateBooleanAssignment = false;
-  
+
   private boolean reverseConversion = false;
 
   private BindingConvertor conversion;
+
+  private PropertyChangeListener forwardListener, reverseListener;
+
+  private Stack<Method> getterMethods = new Stack<Method>();
+
+  private Method sourceGetterMethod, targetGetterMethod;
+
+  private static final Log logger = LogFactory.getLog(Binding.class);
 
   public enum Type {
     ONE_WAY, BI_DIRECTIONAL
@@ -113,12 +130,10 @@ public class Binding {
     }
     return val;
   }
-  
-  public Object doConversions(Object val, Direction dir){
-    if(conversion != null){
-      return (dir == Direction.FORWARD) ? 
-          conversion.sourceToTarget(val):
-          conversion.targetToSource(val);
+
+  public Object doConversions(Object val, Direction dir) {
+    if (conversion != null) {
+      return (dir == Direction.FORWARD) ? conversion.sourceToTarget(val) : conversion.targetToSource(val);
     }
     return val;
   }
@@ -132,13 +147,84 @@ public class Binding {
   }
 
   public boolean isReverseConversion() {
-  
+
     return reverseConversion;
   }
 
   public void setReverseConversion(boolean reverseConversion) {
-  
+
     this.reverseConversion = reverseConversion;
+  }
+
+  public void fireSourceChanged() throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+    Object getRetVal = sourceGetterMethod.invoke(source);
+    forwardListener.propertyChange(new PropertyChangeEvent(getSource(), getSourceAttr(), null, getRetVal));
+  }
+
+  public void bindForward() {
+    setForwardListener(setupBinding(getSource(), getSourceAttr(), getTarget(), getTargetAttr(), Direction.FORWARD));
+    sourceGetterMethod = getterMethods.pop();
+    logger.info("Forward binding established: " + source + "." + sourceAttr + " ==> " + target + "." + targetAttr);
+  }
+
+  public void bindReverse() {
+    setReverseListener(setupBinding(getTarget(), getTargetAttr(), getSource(), getSourceAttr(), Direction.BACK));
+    targetGetterMethod = getterMethods.pop();
+    logger.info("Reverse binding established: " + source + "." + sourceAttr + " <== " + target + "." + targetAttr);
+  }
+
+  private PropertyChangeListener setupBinding(final Object a, final String va, final Object b, final String vb,
+      final Direction dir) {
+    if (!(a instanceof XulEventSource)) {
+      throw new BindingException("Binding error, source object not a XulEventSource instance");
+    }
+    if (a == null || va == null) {
+      throw new BindingException("source bean or property is null");
+    }
+    if (b == null || vb == null) {
+      throw new BindingException("target bean or property is null");
+    }
+    Method sourceGetMethod = BindingUtil.findGetMethod(a, va);
+
+    Class getterClazz = BindingUtil.getMethodReturnType(sourceGetMethod, a);
+    getterMethods.push(sourceGetMethod);
+
+    //find set method
+    final Method targetSetMethod = BindingUtil.findSetMethod(b, vb, getterClazz);
+
+    //setup prop change listener to handle binding
+    PropertyChangeListener listener = new PropertyChangeListener() {
+      public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equalsIgnoreCase(va)) {
+          try {
+            Object value = evaluateExpressions(evt.getNewValue());
+            value = doConversions(value, dir);
+            targetSetMethod.invoke(b, value);
+          } catch (Exception e) {
+            throw new BindingException("Error invoking setter method [" + targetSetMethod.getName() + "]", e);
+          }
+        }
+      }
+    };
+    ((XulEventSource) a).addPropertyChangeListener(listener);
+
+    return listener;
+  }
+
+  public PropertyChangeListener getForwardListener() {
+    return forwardListener;
+  }
+
+  public void setForwardListener(PropertyChangeListener forwardListener) {
+    this.forwardListener = forwardListener;
+  }
+
+  public PropertyChangeListener getReverseListener() {
+    return reverseListener;
+  }
+
+  public void setReverseListener(PropertyChangeListener reverseListener) {
+    this.reverseListener = reverseListener;
   }
 
 }
--- src/org/pentaho/ui/xul/binding/BindingContext.java
+++ src/org/pentaho/ui/xul/binding/BindingContext.java
@@ -1,9 +1,6 @@
 package org.pentaho.ui.xul.binding;
 
-import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,7 +11,6 @@ import org.apache.commons.logging.LogFactory;
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulDomContainer;
 import org.pentaho.ui.xul.XulEventSource;
-import org.pentaho.ui.xul.binding.BindingConvertor.Direction;
 
 public class BindingContext {
 
@@ -23,10 +19,9 @@ public class BindingContext {
   private List<Binding> bindings = new ArrayList<Binding>();
 
   private static final Log logger = LogFactory.getLog(BindingContext.class);
-  
+
   //internal map of Binding to PropChangeListeners, used to cleanup upon removal
-  private Map< Binding, List<PropertyChangeListener> > bindingListeners 
-      = new HashMap< Binding, List<PropertyChangeListener> >();
+  private Map<Binding, List<PropertyChangeListener>> bindingListeners = new HashMap<Binding, List<PropertyChangeListener>>();
 
   public BindingContext(XulDomContainer container) {
     this.container = container;
@@ -38,190 +33,43 @@ public class BindingContext {
     Binding newBinding = new Binding(source, expression.sourceAttr, target, expression.targetAttr);
     add(newBinding);
   }
-  
-  public void remove(Binding bind){
-    if(!bindingListeners.containsKey(bind) && !bindings.contains(bind)){
+
+  public void remove(Binding bind) {
+    if (!bindingListeners.containsKey(bind) && !bindings.contains(bind)) {
       return;
     }
-    
-    for(PropertyChangeListener propListener : bindingListeners.get(bind)){
-      if(bind.getSource() instanceof XulEventSource){
+
+    for (PropertyChangeListener propListener : bindingListeners.get(bind)) {
+      if (bind.getSource() instanceof XulEventSource) {
         ((XulEventSource) bind.getSource()).removePropertyChangeListener(propListener);
       }
-      
-      if(bind.getTarget() instanceof XulEventSource){
+
+      if (bind.getTarget() instanceof XulEventSource) {
         ((XulEventSource) bind.getTarget()).removePropertyChangeListener(propListener);
       }
     }
     bindingListeners.remove(bind);
     bindings.remove(bind);
-    
+
   }
 
   public void add(Binding bind) {
     try {
       bindings.add(bind);
-      //forward binding
-      setupBinding(bind, bind.getSource(), bind.getSourceAttr(), bind.getTarget(), bind.getTargetAttr(),
-          Direction.FORWARD);
+      bind.bindForward();
 
-      //reverse binding
+      if (!bindingListeners.containsKey(bind)) {
+        bindingListeners.put(bind, new ArrayList<PropertyChangeListener>());
+      }
+      bindingListeners.get(bind).add(bind.getForwardListener());
+
       if (bind.getBindingType() == Binding.Type.BI_DIRECTIONAL) {
-        logger.info("Bi-directional");
-        setupBinding(bind, bind.getTarget(), bind.getTargetAttr(), bind.getSource(), bind.getSourceAttr(),
-            Direction.BACK);
-      } else {
-        logger.info("Uni-directional");
+        bind.bindReverse();
+        bindingListeners.get(bind).add(bind.getReverseListener());
       }
     } catch (Throwable t) {
       throw new BindingException("Binding failed: " + bind.getSource() + "." + bind.getSourceAttr() + " <==> "
           + bind.getTarget() + "." + bind.getTargetAttr(), t);
     }
   }
-
-  private Method findGetMethod(Object o, String property) {
-    String methodName = null;
-    try {
-      methodName = "get" + (String.valueOf(property.charAt(0)).toUpperCase()) + property.substring(1);
-      Method getMethod = o.getClass().getMethod(methodName);
-      return getMethod;
-    } catch (NoSuchMethodException e) {
-      logger.debug("could not resolve getter method [" + methodName + "] for property [" + property + "] on object ["
-          + o.getClass().getName() + "].  Trying to resolve as boolean style getter...");
-      try {
-        String isMethodName = "is" + (String.valueOf(property.charAt(0)).toUpperCase()) + property.substring(1);
-        Method getMethod = o.getClass().getMethod(isMethodName);
-        return getMethod;
-      } catch (NoSuchMethodException ex) {
-        throw new BindingException("Could not resolve getter method for property [" + property + "] on object ["
-            + o.getClass().getName() + "]", ex);
-      }
-    }
-  }
-
-  private Method findSetMethod(Object o, String property, Class paramClass) {
-    String methodName = "set" + (String.valueOf(property.charAt(0)).toUpperCase()) + property.substring(1);
-
-    try {
-      if (paramClass != null) {
-        //try to resolve by name and the Type of object returned by the getter
-        Method setMethod = o.getClass().getMethod(methodName, paramClass);
-        logger.debug("Found set method by name and type");
-        return setMethod;
-      } else {
-        return getMethodNameByNameOnly(o, methodName);
-      }
-    } catch (Exception e) {
-      try{
-        return getMethodNameByNameOnly(o, methodName);
-      } catch (BindingException ex){
-        throw new BindingException("Could not resolve setter method for property [" + property + "] on object ["
-            + o.getClass().getName() + "] with paramClass: "+paramClass, e);
-      }
-    }
-  }
-  
-  private Method getMethodNameByNameOnly(Object o, String methodName) throws BindingException{
-    for (Method m : o.getClass().getMethods()) {
-      //just match on name
-      if (m.getName().equals(methodName)) {
-        return m;
-      }
-    }
-    throw new BindingException("Could not resolve method by name either. You're really hosed, check property name");
-  }
-
-  private Class getObjectClassOrType(Object o) {
-    if (o instanceof Boolean) {
-      return Boolean.TYPE;
-    } else if (o instanceof Integer) {
-      return Integer.TYPE;
-    } else if (o instanceof Float) {
-      return Float.TYPE;
-    } else if (o instanceof Double) {
-      return Double.TYPE;
-    } else if (o instanceof Short) {
-      return Short.TYPE;
-    } else if (o instanceof Long) {
-      return Long.TYPE;
-    } else {
-      return o.getClass();
-    }
-  }
-
-  private void setupBinding(final Binding bind, final Object source, final String sourceAttr,
-      final Object target, final String targetAttr, final Direction dir) {
-    
-
-    if(!(source instanceof XulEventSource)){
-      logger.error("Binding error, source object not a XulEventSource instance");
-      return;
-    }
-    
-    if (source == null || sourceAttr == null) {
-      throw new BindingException("source bean or property is null");
-    }
-    if (target == null || targetAttr == null) {
-      throw new BindingException("target bean or property is null");
-    }
-    Method sourceGetMethod = findGetMethod(source, sourceAttr);
-
-    //get class of object returned by getter
-    Class getClazz = null;
-    Object getRetVal = null;
-    try {
-      getRetVal = sourceGetMethod.invoke(source);
-      logger.debug("Found get Return Value: " + getRetVal);
-    } catch (IllegalAccessException e) {
-      throw new BindingException("Error invoking getter method [" + sourceGetMethod.getName() + "]", e);
-    } catch (InvocationTargetException e) {
-      throw new BindingException("Error invoking getter method [" + sourceGetMethod.getName() + "]", e);
-    }
-    if (getRetVal != null) {
-      logger.debug("Get Return was not null, checking it's type");
-      
-      if(bind.getConversion() != null){
-        logger.debug("Conversion Object found, converting 'get' return value before looking for set");
-        getRetVal = bind.doConversions(getRetVal, dir);
-      }
-      
-      //checks for Boxed primatives
-      getClazz = getObjectClassOrType(getRetVal);
-      logger.debug("Get Return type is: " + getClazz);
-    } 
-
-    //find set method
-    final Method targetSetMethod = findSetMethod(target, targetAttr, getClazz);
-
-    
-    
-    //setup prop change listener to handle binding
-    PropertyChangeListener listener = new PropertyChangeListener() {
-      public void propertyChange(PropertyChangeEvent evt) {
-        if (evt.getPropertyName().equalsIgnoreCase(sourceAttr)) {
-          try {
-            Object value = bind.evaluateExpressions(evt.getNewValue());
-            value = bind.doConversions(value, dir);
-            targetSetMethod.invoke(target, value);
-          } catch (Exception e) {
-            throw new BindingException("Error invoking setter method [" + targetSetMethod.getName() + "]", e);
-          }
-        }
-      }
-    };
-
-    ((XulEventSource) source).addPropertyChangeListener(listener);
-
-    //add listener to map so we can remove it later when asked.
-    if(! bindingListeners.containsKey(bind)){
-      bindingListeners.put(bind, new ArrayList<PropertyChangeListener>());
-    }
-    
-    bindingListeners.get(bind).add(listener);
-
-    logger.info("Binding established: " + source + "." + sourceAttr + " ==> " + target + "." + targetAttr);
-  
-  
-  }
-
 }
