90f5374d4b131a34d3eb4f8c9afd77050d8a1b47
Aaron Phillips <aphillips@pentaho.com>
2008-06-24 16:00:36 +0000

    added debug

--- src/org/pentaho/ui/xul/swing/tags/SwingTreeCell.java
+++ src/org/pentaho/ui/xul/swing/tags/SwingTreeCell.java
@@ -13,82 +13,84 @@ import org.pentaho.ui.xul.swing.SwingElement;
 import org.pentaho.ui.xul.dom.Element;
 
 public class SwingTreeCell extends SwingElement implements XulTreeCell {
-	
-	private JLabel label;
-	private Object value = null;
-	private int index = 0;
-	private static final Log logger = LogFactory.getLog(SwingTreeCell.class);
-	
-	public SwingTreeCell(Element self, XulComponent parent, XulDomContainer domContainer, String tagName) {
-		super("treecell");
-		label = new JLabel();
-		managedObject = label;
-	}
-	
-	public SwingTreeCell(XulComponent parent) {
-		super("treecell");
-		label = new JLabel();
-		managedObject = label;
-	}
-	
-	public void setSelectedIndex(int index){
-		this.index = index;
-	}
-	
-	public int getSelectedIndex(){
-		return index;
-	}
-	
-	public String getLabel() {
-		return label.getText();
-	}
 
-	public String getSrc() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  private JLabel label;
 
-	public Object getValue() {
-		return value;
-	}
+  private Object value = null;
 
-	public boolean isEditable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+  private int index = 0;
+  private static final Log logger = LogFactory.getLog(SwingTreeCell.class);
 
-	public void setEditable(boolean edit) {
-		// TODO Auto-generated method stub
-		
-	}
+  public SwingTreeCell(Element self, XulComponent parent, XulDomContainer domContainer, String tagName) {
+    super("treecell");
+    label = new JLabel();
+    managedObject = label;
+  }
 
-	public void setLabel(String label) {
-		this.label.setText(label);
-	}
+  public SwingTreeCell(XulComponent parent) {
+    super("treecell");
+    label = new JLabel();
+    managedObject = label;
+  }
 
-	public void setSrc(String srcUrl) {
-		// TODO Auto-generated method stub
-		
-	}
+  public void setSelectedIndex(int index) {
+    logger.debug("swingTreeCell.setSelectedIndex("+index+")");
+    this.index = index;
+  }
 
-	public void setValue(Object value) {
-		if(value instanceof String && ((String) value).indexOf(",") == -1){
-		  //String and not a comma separated list
-			this.value = Boolean.parseBoolean(((String) value));
-		} else if(value instanceof String && ((String) value).indexOf(",") > -1){
+  public int getSelectedIndex() {
+    return index;
+  }
+
+  public String getLabel() {
+    return label.getText();
+  }
+
+  public String getSrc() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object getValue() {
+    return value;
+  }
+
+  public boolean isEditable() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public void setEditable(boolean edit) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void setLabel(String label) {
+    this.label.setText(label);
+  }
+
+  public void setSrc(String srcUrl) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void setValue(Object value) {
+    if (value instanceof String && ((String) value).indexOf(",") == -1) {
+      //String and not a comma separated list
+      this.value = Boolean.parseBoolean(((String) value));
+    } else if (value instanceof String && ((String) value).indexOf(",") > -1) {
       //String and a comma separated list
       String[] list = ((String) value).split(",");
       Vector<String> vec = new Vector<String>();
-      for(String item : list){
+      for (String item : list) {
         vec.add(item);
       }
       this.value = vec;
-      
-    } else if(value instanceof Boolean){
-			this.value = (Boolean) value;
-		} else {
-			this.value = value;
-		}
-	}
 
+    } else if (value instanceof Boolean) {
+      this.value = (Boolean) value;
+    } else {
+      this.value = value;
+    }
+  }
 }
