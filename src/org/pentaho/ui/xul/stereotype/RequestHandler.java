4edb456f545b0d7125afcb62cf819d42ea5b8aca
Aaron Phillips <aphillips@pentaho.com>
2008-07-10 14:43:53 +0000

    added @RequestHandler annotation to denote methods in a controller that respond to XUL events

---
+++ src/org/pentaho/ui/xul/stereotype/RequestHandler.java
@@ -0,0 +1,9 @@
+package org.pentaho.ui.xul.stereotype;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+public @interface RequestHandler {
+
+}
