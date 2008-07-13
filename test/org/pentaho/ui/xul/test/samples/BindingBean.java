20d604bfa41e5b4bc1375cadf4e488315d7ca712
Aaron Phillips <aphillips@pentaho.com>
2008-07-13 19:39:22 +0000

    added more robust event firing in BindingBean (test utility)

--- test/org/pentaho/ui/xul/test/samples/BindingBean.java
+++ test/org/pentaho/ui/xul/test/samples/BindingBean.java
@@ -2,26 +2,37 @@ package org.pentaho.ui.xul.test.samples;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.List;
 
 import org.pentaho.ui.xul.XulEventSource;
+import org.pentaho.ui.xul.util.XulEventSourceAdapter;
 
-public class BindingBean implements XulEventSource {
+public class BindingBean extends XulEventSourceAdapter {
 
   private String property1, property2;
 
+  private List<String> listProperty;
+  
+  private List<BindingBean> bindingBeans;
 
   protected PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
 
-  public void addPropertyChangeListener(PropertyChangeListener listener) {
-    changeSupport.addPropertyChangeListener(listener);
+  public List<BindingBean> getBindingBeans() {
+    return bindingBeans;
   }
 
-  public void removePropertyChangeListener(PropertyChangeListener listener) {
-    changeSupport.removePropertyChangeListener(listener);
+  public void setBindingBeans(List<BindingBean> bindingBeans) {
+    this.bindingBeans = bindingBeans;
+    firePropertyChange("bindingBeans", null, bindingBeans);
   }
 
-  protected void firePropertyChange(String attr, Object previousVal, Object newVal) {
-    changeSupport.firePropertyChange(attr, previousVal, newVal);
+  public List<String> getListProperty() {
+    return listProperty;
+  }
+
+  public void setListProperty(List<String> listProperty) {
+    this.listProperty = listProperty;
+    firePropertyChange("listProperty", null, listProperty);
   }
 
   public String getProperty1() {
@@ -29,8 +40,9 @@ public class BindingBean implements XulEventSource {
   }
 
   public void setProperty1(String property1) {
+    Object oldVal = this.property1;
     this.property1 = property1;
-    firePropertyChange("property1", null, property1);
+    firePropertyChange("property1", oldVal, property1);
   }
 
   public String getProperty2() {
@@ -38,8 +50,8 @@ public class BindingBean implements XulEventSource {
   }
 
   public void setProperty2(String property2) {
+    Object oldVal = this.property2;
     this.property2 = property2;
-    firePropertyChange("property2", null, property1);
+    firePropertyChange("property2", oldVal, property1);
   }
-
 }
