21992e08353ca937bb0bb019be068862c7e270b8
Aaron Phillips <aphillips@pentaho.com>
2008-12-08 15:51:05 +0000

    added trunk

--- src/org/pentaho/ui/xul/html/IHtmlElement.java
+++
@@ -1,9 +0,0 @@
-package org.pentaho.ui.xul.html;
-
-public interface IHtmlElement {
-
-	public void getHtml( StringBuilder sb );
-	
-	public void getScript( StringBuilder sb );
-	
-}
--- src/org/pentaho/ui/xul/util/TextType.java
+++
@@ -1,7 +0,0 @@
-package org.pentaho.ui.xul.util;
-
-public enum TextType {
-  
-  NORMAL, PASSWORD, AUTOCOMPLETE, NUMERIC, TIMED;
-
-}
