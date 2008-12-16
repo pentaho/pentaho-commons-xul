4b5ac7ab92a2d2ff622c11b406c07cf4df3bedc5
Aaron Phillips <aphillips@pentaho.com>
2008-12-16 22:35:27 +0000

    GwtOverlay now implements the correct XulOverlay interface. the old one is removed

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtOverlay.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtOverlay.java
@@ -1,9 +1,8 @@
 package org.pentaho.ui.xul.gwt.tags;
 
 import org.pentaho.ui.xul.XulComponent;
-import org.pentaho.ui.xul.components.XulOverlay;
+import org.pentaho.ui.xul.XulOverlay;
 import org.pentaho.ui.xul.dom.Element;
-import org.pentaho.ui.xul.gwt.AbstractGwtXulComponent;
 import org.pentaho.ui.xul.gwt.AbstractGwtXulContainer;
 import org.pentaho.ui.xul.gwt.GwtXulHandler;
 import org.pentaho.ui.xul.gwt.GwtXulParser;
@@ -48,6 +47,21 @@ public class GwtOverlay extends AbstractGwtXulContainer implements XulOverlay{
   @Override
   public void layout() {
     //No layout required
+  }
+
+  public String getOverlayUri() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String getResourceBundleUri() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String getSource() {
+    // TODO Auto-generated method stub
+    return null;
   }
 
   
