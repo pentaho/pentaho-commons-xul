86640a72b06db7363ccf957b97e56f7fa98d85c3
Will Gorman <wgorman@pentaho.com>
2008-12-18 23:45:48 +0000

    added command attrib when initing

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtCheckbox.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtCheckbox.java
@@ -40,6 +40,7 @@ public class GwtCheckbox extends AbstractGwtXulComponent implements XulCheckbox 
     setLabel(srcEle.getAttribute("label"));
     setChecked("true".equals(srcEle.getAttribute("checked")));
     setDisabled("true".equals(srcEle.getAttribute("disabled")));
+    setCommand(srcEle.getAttribute("command"));
   }
   
   /* (non-Javadoc)
