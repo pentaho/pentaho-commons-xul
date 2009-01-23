076356249e63b265a2d24ec85b76f160fc36fa4d
Will Gorman <wgorman@pentaho.com>
2009-01-23 23:04:06 +0000

    converted list item to use underlying dom for attributes vs. separate storage

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtListitem.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtListitem.java
@@ -1,5 +1,6 @@
 package org.pentaho.ui.xul.gwt.tags;
 
+import org.pentaho.gwt.widgets.client.utils.StringUtils;
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulDomContainer;
 import org.pentaho.ui.xul.components.XulListitem;
@@ -11,6 +12,8 @@ import org.pentaho.ui.xul.gwt.GwtXulParser;
 public class GwtListitem extends AbstractGwtXulComponent implements XulListitem {
   
   static final String ELEMENT_NAME = "listitem"; //$NON-NLS-1$
+  private static final String VALUE = "value"; //$NON-NLS-1$
+  private static final String LABEL = "label"; //$NON-NLS-1$
   
   public static void register() {
     GwtXulParser.registerHandler(ELEMENT_NAME, 
@@ -21,39 +24,40 @@ public class GwtListitem extends AbstractGwtXulComponent implements XulListitem 
     });
   }
   
-  private String label;
-  private Object value;
-  
   public GwtListitem() {
     super(ELEMENT_NAME);
   }
   
   public void init(com.google.gwt.xml.client.Element srcEle, XulDomContainer container) {
     super.init(srcEle, container);
-    setLabel(srcEle.getAttribute("label"));
-    setValue(srcEle.getAttribute("value"));
+    if (!StringUtils.isEmpty(srcEle.getAttribute(LABEL))) {
+      setLabel(srcEle.getAttribute(LABEL));
+    }
+    if (!StringUtils.isEmpty(srcEle.getAttribute(VALUE))) {
+      setValue(srcEle.getAttribute(VALUE));
+    }
   }
+
   public String getLabel(){
-    return label;
+    return getAttributeValue(LABEL);
   }
   
-  public void setLabel(String label){
-    this.label = label;
-    managedObject = this.label;
+  public void setLabel(String label) {
+    setAttribute(LABEL, label);
   }
   
   public String toString(){
-    return this.label;
+    return getLabel();
   }
   
   public void layout(){
   }
 
   public Object getValue() {
-    if (value != null) {
-      return value;
+    if (getAttributeValue(VALUE) != null) {
+      return getAttributeValue(VALUE);
     }
-    return label;
+    return getLabel();
   }
 
   public boolean isSelected() {
@@ -64,13 +68,17 @@ public class GwtListitem extends AbstractGwtXulComponent implements XulListitem 
   }
 
   public void setValue(Object value) {
-    this.value = value;
+    setAttribute(VALUE, (String)value);
   }
 
   public void adoptAttributes(XulComponent component) {
     
-    if(component.getAttributeValue("label") != null){
-      setLabel(component.getAttributeValue("label"));
+    if(component.getAttributeValue(LABEL) != null){
+      setLabel(component.getAttributeValue(LABEL));
+    }
+    
+    if(component.getAttributeValue(VALUE) != null){
+      setValue(component.getAttributeValue(VALUE));
     }
   }
 }
