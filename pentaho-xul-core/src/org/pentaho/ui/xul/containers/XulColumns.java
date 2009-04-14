8aff4c75d6e79a876f80cc18f9abae7bc50df3e6
Nick Baker <nbaker@pentaho.com>
2009-04-14 16:07:25 +0000

    Added Grid interfaces

---
+++ pentaho-xul-core/src/org/pentaho/ui/xul/containers/XulColumns.java
@@ -0,0 +1,10 @@
+package org.pentaho.ui.xul.containers;
+
+import org.pentaho.ui.xul.XulContainer;
+
+/**
+ * User: nbaker
+ * Date: Apr 14, 2009
+ */
+public interface XulColumns extends XulContainer {
+}
