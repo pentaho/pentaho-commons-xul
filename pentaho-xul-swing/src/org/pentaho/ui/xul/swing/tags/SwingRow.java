ff4a4e3b39669ca311a3c7065e4a709802dbed84
William Seyler <wseyler@pentaho.com>
2009-04-15 14:00:29 +0000

    Fix for SwingRows and SwingRow

--- pentaho-xul-swing/src/org/pentaho/ui/xul/swing/tags/SwingRow.java
+++ pentaho-xul-swing/src/org/pentaho/ui/xul/swing/tags/SwingRow.java
@@ -2,6 +2,7 @@ package org.pentaho.ui.xul.swing.tags;
 
 import org.pentaho.ui.xul.swing.AbstractSwingContainer;
 import org.pentaho.ui.xul.containers.XulColumns;
+import org.pentaho.ui.xul.containers.XulRow;
 import org.pentaho.ui.xul.dom.Element;
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulDomContainer;
@@ -10,7 +11,7 @@ import org.pentaho.ui.xul.XulDomContainer;
  * User: nbaker
  * Date: Apr 14, 2009
  */
-public class SwingRow extends AbstractSwingContainer implements XulColumns {
+public class SwingRow extends AbstractSwingContainer implements XulColumns, XulRow {
 
   public SwingRow(Element self, XulComponent parent, XulDomContainer domContainer, String tagName) {
 		super("row");
--- pentaho-xul-swing/src/org/pentaho/ui/xul/swing/tags/SwingRows.java
+++ pentaho-xul-swing/src/org/pentaho/ui/xul/swing/tags/SwingRows.java
@@ -2,6 +2,7 @@ package org.pentaho.ui.xul.swing.tags;
 
 import org.pentaho.ui.xul.swing.AbstractSwingContainer;
 import org.pentaho.ui.xul.containers.XulColumns;
+import org.pentaho.ui.xul.containers.XulRows;
 import org.pentaho.ui.xul.dom.Element;
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulDomContainer;
@@ -10,7 +11,7 @@ import org.pentaho.ui.xul.XulDomContainer;
  * User: nbaker
  * Date: Apr 14, 2009
  */
-public class SwingRows extends AbstractSwingContainer implements XulColumns {
+public class SwingRows extends AbstractSwingContainer implements XulColumns, XulRows {
 
   public SwingRows(Element self, XulComponent parent, XulDomContainer domContainer, String tagName) {
 		super("rows");
