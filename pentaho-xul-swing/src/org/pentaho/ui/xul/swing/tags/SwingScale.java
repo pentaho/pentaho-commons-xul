3085753851a184139a523f7f43ac39322a70ceb7
Nick Baker <nbaker@pentaho.com>
2009-04-21 12:40:33 +0000

    Removed JDK @Override annotation

--- pentaho-xul-swing/src/org/pentaho/ui/xul/swing/tags/SwingScale.java
+++ pentaho-xul-swing/src/org/pentaho/ui/xul/swing/tags/SwingScale.java
@@ -26,61 +26,48 @@ public class SwingScale extends SwingElement implements XulScale{
     super("scale");
   }
   
-
-  @Override
   public String getDir() {
     return direction;
   }
 
-  @Override
   public int getMax() {
     return max;
   }
 
-  @Override
   public int getMin() {
     return min;
   }
 
-  @Override
   public int getPageincrement() {
     return increment;
   }
 
-  @Override
   public int getValue() {
     return value;
   }
 
-  @Override
   public void setDir(String direction) {
     this.direction = direction;
   }
 
-  @Override
   public void setMax(int max) {
     this.max = max;
   }
 
-  @Override
   public void setMin(int min) {
     this.min = min;
   }
 
-  @Override
   public void setPageincrement(int increment) {
     this.increment = increment;
   }
 
-  @Override
   public void setValue(int value) {
     int prevVal = this.value;
     this.value = value;
     this.changeSupport.firePropertyChange("value", prevVal, value);
   }
 
-
-  @Override
   public void layout() {
     int orient = (orientation == Orient.VERTICAL)? JSlider.VERTICAL: JSlider.HORIZONTAL;
     final JSlider slider = new JSlider(orient, this.min, this.max, Math.max(min, this.value));
@@ -93,7 +80,7 @@ public class SwingScale extends SwingElement implements XulScale{
     
     slider.addChangeListener(new ChangeListener(){
 
-      @Override
+      
       public void stateChanged(ChangeEvent arg0) {
         setValue(slider.getValue());
       }
@@ -101,8 +88,4 @@ public class SwingScale extends SwingElement implements XulScale{
     });
 
   }
-  
-  
-
-  
 }
