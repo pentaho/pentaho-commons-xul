bc5c9894cdc68897bfc5608e295ac6c092f49911
Nick Baker <nbaker@pentaho.com>
2009-06-19 19:23:23 +0000

    Temporarily backing out layout logic.

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/AbstractGwtXulContainer.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/AbstractGwtXulContainer.java
@@ -4,8 +4,6 @@ import java.util.Iterator;
 
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulContainer;
-import org.pentaho.ui.xul.XulDomException;
-import org.pentaho.ui.xul.dom.Element;
 import org.pentaho.ui.xul.util.Align;
 
 import com.google.gwt.user.client.ui.Widget;
@@ -13,7 +11,6 @@ import com.google.gwt.user.client.ui.Widget;
 public abstract class AbstractGwtXulContainer extends AbstractGwtXulComponent implements XulContainer{
 
   protected Align alignment;
-  protected boolean suppressLayout;
   
   public AbstractGwtXulContainer(String tagName){
     super(tagName);
@@ -24,62 +21,14 @@ public abstract class AbstractGwtXulContainer extends AbstractGwtXulComponent im
     if(initialized == false){
       layout();
     }
+
+  if (initialized) {
+    resetContainer();
+    layout();
+  }
+
+  }
   
-    if (initialized) {
-      resetContainer();
-      layout();
-    }
-  
-  }
-
-  
-  
-  @Override
-  public void addChild(Element element) {
-    super.addChild(element);
-    if (initialized) {
-      resetContainer();
-      layout();
-    }
-  }
-
-  @Override
-  public void addChildAt(Element element, int idx) {
-    super.addChildAt(element, idx);
-    if (initialized) {
-      resetContainer();
-      layout();
-    }
-  }
-
-  @Override
-  public void removeChild(Element element) {
-    super.removeChild(element);
-    if (initialized) {
-      resetContainer();
-      layout();
-    }
-  }
-
-  @Override
-  public void replaceChild(Element oldElement, Element newElement) {
-    super.replaceChild(oldElement, newElement);
-    if (initialized) {
-      resetContainer();
-      layout();
-    }
-  }
-
-  @Override
-  public void replaceChild(XulComponent oldElement, XulComponent newElement)
-      throws XulDomException {
-    super.replaceChild(oldElement, newElement);
-    if (initialized) {
-      resetContainer();
-      layout();
-    }
-  }
-
   public void addComponentAt(XulComponent component, int idx) {
     this.addChildAt(component, idx);
     if (initialized) {
@@ -107,11 +56,6 @@ public abstract class AbstractGwtXulContainer extends AbstractGwtXulComponent im
       }
     }
   }
-
-  public void suppressLayout(boolean suppress) {
-    this.suppressLayout = suppress;
-  }
-  
 }
 
   
