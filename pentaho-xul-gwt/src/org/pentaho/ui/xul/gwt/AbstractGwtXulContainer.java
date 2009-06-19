7242fe3db40912815e73e5cbe95268c14a8d251f
Nick Baker <nbaker@pentaho.com>
2009-06-19 15:00:55 +0000

    calling layout when childNodes changes

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/AbstractGwtXulContainer.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/AbstractGwtXulContainer.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 
 import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.XulContainer;
+import org.pentaho.ui.xul.XulDomException;
+import org.pentaho.ui.xul.dom.Element;
 import org.pentaho.ui.xul.util.Align;
 
 import com.google.gwt.user.client.ui.Widget;
@@ -22,14 +24,62 @@ public abstract class AbstractGwtXulContainer extends AbstractGwtXulComponent im
     if(initialized == false){
       layout();
     }
-
-  if (initialized) {
-    resetContainer();
-    layout();
-  }
-
-  }
   
+    if (initialized) {
+      resetContainer();
+      layout();
+    }
+  
+  }
+
+  
+  
+  @Override
+  public void addChild(Element element) {
+    super.addChild(element);
+    if (initialized) {
+      resetContainer();
+      layout();
+    }
+  }
+
+  @Override
+  public void addChildAt(Element element, int idx) {
+    super.addChildAt(element, idx);
+    if (initialized) {
+      resetContainer();
+      layout();
+    }
+  }
+
+  @Override
+  public void removeChild(Element element) {
+    super.removeChild(element);
+    if (initialized) {
+      resetContainer();
+      layout();
+    }
+  }
+
+  @Override
+  public void replaceChild(Element oldElement, Element newElement) {
+    super.replaceChild(oldElement, newElement);
+    if (initialized) {
+      resetContainer();
+      layout();
+    }
+  }
+
+  @Override
+  public void replaceChild(XulComponent oldElement, XulComponent newElement)
+      throws XulDomException {
+    super.replaceChild(oldElement, newElement);
+    if (initialized) {
+      resetContainer();
+      layout();
+    }
+  }
+
   public void addComponentAt(XulComponent component, int idx) {
     this.addChildAt(component, idx);
     if (initialized) {
