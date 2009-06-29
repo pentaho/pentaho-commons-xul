bd12de974fc8a9ddaeccf3231a7f0281469f4c81
Ramaiz Mansoor <rmansoor@pentaho.com>
2009-06-29 15:58:58 +0000

    Fixed the tab panel css to base xul tabs

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/widgets/GwtTabWidget.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/widgets/GwtTabWidget.java
@@ -3,22 +3,19 @@ package org.pentaho.ui.xul.gwt.widgets;
 import org.pentaho.gwt.widgets.client.utils.ElementUtils;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PopupPanel;
-import com.google.gwt.user.client.ui.SourcesTabEvents;
-import com.google.gwt.user.client.ui.TabListener;
+import com.google.gwt.user.client.ui.MouseListener;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class GwtTabWidget extends HorizontalPanel {
-
-  private static final int TAB_TEXT_LENGTH = 12;
-
+public class GwtTabWidget extends HorizontalPanel implements MouseListener {
   private TabPanel tabPanel;
   private Widget tabContent;
   private Label textLabel = new Label();
@@ -31,8 +28,8 @@ public class GwtTabWidget extends HorizontalPanel {
     this.tabContent = tabContent;
     this.fullText = text;
     setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
-    panel.setStyleName("tabWidget"); //$NON-NLS-1$
-    leftCap.setStyleName("tabWidgetCap"); //$NON-NLS-1$
+    panel.setStyleName("xulTabWidget"); //$NON-NLS-1$
+    leftCap.setStyleName("xulTabWidgetCap"); //$NON-NLS-1$
     Image leftCapImage = new Image(GWT.getModuleBaseURL() + "/image/tableft.gif");
     leftCap.setSpacing(0);
     leftCapImage.setWidth("5px"); //$NON-NLS-1$
@@ -40,30 +37,27 @@ public class GwtTabWidget extends HorizontalPanel {
 
     setLabelText(text);
     setLabelTooltip(tooltip);
+    textLabel.addStyleName("xulTabWidgetLabel");
     textLabel.setWordWrap(false);
+    textLabel.addMouseListener(this);
+    tabPanel.addSelectionHandler(new SelectionHandler<Integer>() {
 
-    tabPanel.addTabListener(new TabListener() {
-
-      public boolean onBeforeTabSelected(SourcesTabEvents sender, int tabIndex) {
-        return true;
-      }
-
-      public void onTabSelected(SourcesTabEvents sender, int tabIndex) {
+      public void onSelection(SelectionEvent<Integer> event) {
+        int tabIndex = event.getSelectedItem();
         ElementUtils.blur(getElement().getParentElement());
         if (tabIndex == tabPanel.getWidgetIndex(tabContent)) {
-          panel.setStyleName("tabWidget-selected"); //$NON-NLS-1$
-          leftCap.setStyleName("tabWidgetCap-selected"); //$NON-NLS-1$
+          panel.setStyleName("xulTabWidget-selected"); //$NON-NLS-1$
+          leftCap.setStyleName("xulTabWidgetCap-selected"); //$NON-NLS-1$
         } else {
-          panel.setStyleName("tabWidget"); //$NON-NLS-1$
-          leftCap.setStyleName("tabWidgetCap"); //$NON-NLS-1$
+          panel.setStyleName("xulTabWidget"); //$NON-NLS-1$
+          leftCap.setStyleName("xulTabWidgetCap"); //$NON-NLS-1$
         }
       }
-
     });
 
     panel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
     panel.add(textLabel);
-    DOM.setStyleAttribute(textLabel.getElement(), "margin", "4px 5px 5px 5px"); //$NON-NLS-1$ //$NON-NLS-2$
+//    DOM.setStyleAttribute(textLabel.getElement(), "margin", "4px 5px 5px 5px"); //$NON-NLS-1$ //$NON-NLS-2$
     DOM.setStyleAttribute(textLabel.getElement(), "paddingRight", "5px"); //$NON-NLS-1$ //$NON-NLS-2$
     add(leftCap);
     add(panel);
@@ -85,5 +79,33 @@ public class GwtTabWidget extends HorizontalPanel {
   public void setLabelTooltip(String tooltip) {
     textLabel.setTitle(tooltip);
   }
+
+  public void onMouseDown(Widget sender, int x, int y) {
+  }
+  
+  public void onMouseEnter(Widget sender) {
+    if (tabPanel.getTabBar().getSelectedTab() == tabPanel.getWidgetIndex(tabContent)) {
+        // don't do anything
+    } else {
+      panel.setStyleName("xulTabWidget-hover"); //$NON-NLS-1$
+      leftCap.setStyleName("xulTabWidgetCap-hover"); //$NON-NLS-1$
+    }
+  }
+
+  public void onMouseLeave(Widget sender) {
+   if (tabPanel.getTabBar().getSelectedTab() == tabPanel.getWidgetIndex(tabContent)) {
+        // don't do anything
+   } else {
+      panel.setStyleName("xulTabWidget"); //$NON-NLS-1$
+      leftCap.setStyleName("xulTabWidgetCap"); //$NON-NLS-1$
+   }
+  }
+
+  public void onMouseMove(Widget sender, int x, int y) {
+  }
+
+  public void onMouseUp(Widget sender, int x, int y) {
+  }
+
 
 }
