3a7a3e75305f3c9df899f036db390611a3e4bb21
Nick Baker <nbaker@pentaho.com>
2009-11-02 16:31:57 +0000

    Changed name of image binding property

--- pentaho-xul-core/src/org/pentaho/ui/xul/components/XulTreeCol.java
+++ pentaho-xul-core/src/org/pentaho/ui/xul/components/XulTreeCol.java
@@ -62,7 +62,7 @@ public interface XulTreeCol extends XulComponent {
   public String getDisabledbinding();
   public void setDisabledbinding(String property);
 
-  public String getImage();
-  public void setImage(String img);
+  public String getImagebinding();
+  public void setImagebinding(String img);
 
 }
