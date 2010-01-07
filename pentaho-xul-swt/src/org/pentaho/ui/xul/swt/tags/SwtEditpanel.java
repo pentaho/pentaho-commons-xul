26230c532211b7c1b6970fde29cae18e33ae5a86
Nick Baker <nbaker@pentaho.com>
2010-01-07 19:28:51 +0000

    [AGILEBI-101] Images on Collapse control

--- pentaho-xul-swt/src/org/pentaho/ui/xul/swt/tags/SwtEditpanel.java
+++ pentaho-xul-swt/src/org/pentaho/ui/xul/swt/tags/SwtEditpanel.java
@@ -1,6 +1,9 @@
 package org.pentaho.ui.xul.swt.tags;
 
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.ViewForm;
@@ -10,6 +13,8 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -26,10 +31,11 @@ import org.pentaho.ui.xul.dom.Element;
 import org.pentaho.ui.xul.swt.AbstractSwtXulContainer;
 import org.pentaho.ui.xul.swt.SwtElement;
 import org.pentaho.ui.xul.util.Orient;
+import org.pentaho.ui.xul.util.XulUtil;
 
 public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpanel{
 
-  private ViewForm topForm;
+  private Composite topForm;
   private CLabel lbl;
   private ToolItem btn;
   private boolean collapsible;
@@ -38,12 +44,15 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
   private XulToolbar toolbar;
   private boolean collapsed;
   private Composite parentComposite;
-  private int hiddenflex;
-  private String cachedLabel;
+  private int hiddenflex, hiddenWidth;
+  private Composite toolbarPanel, buttonPanel, titlePanel;
+  private Image rightImg, leftImg;
+  private XulDomContainer domContainer;
   
   public SwtEditpanel(Element self, XulComponent parent, XulDomContainer container, String tagName) {
     super(tagName);
     setOrient(Orient.VERTICAL.toString());
+    this.domContainer = container;
     
     parentComposite = (Composite) parent.getManagedObject();
     mainComposite = new Composite(parentComposite, SWT.BORDER);
@@ -52,8 +61,7 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
     data.grabExcessVerticalSpace = true;
     data.horizontalAlignment = SWT.FILL;
     data.grabExcessHorizontalSpace = true;
-//    mainComposite.setLayoutData(data);
-    
+
     GridLayout layout = new GridLayout();
     mainComposite.setLayout(layout);
     layout.verticalSpacing = 0;
@@ -61,7 +69,8 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
     layout.marginWidth = 0;
     layout.marginHeight = 0;
     
-    topForm = new ViewForm(mainComposite, SWT.NONE);
+    
+    topForm = new Composite(mainComposite, SWT.NONE);
     topForm.addPaintListener(new PaintListener(){
 
       public void paintControl(PaintEvent arg0) {
@@ -71,6 +80,37 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
       }
       
     });
+    
+    layout = new GridLayout(2, false);
+    layout.verticalSpacing = 0;
+    layout.horizontalSpacing = 0;
+    layout.marginWidth = 0;
+    layout.marginHeight = 0;
+    topForm.setLayout(layout);
+    
+    GridData gData = new GridData();
+    gData.grabExcessHorizontalSpace = true;
+    gData.grabExcessVerticalSpace = true;
+    gData.horizontalAlignment = SWT.FILL;
+    gData.verticalAlignment = SWT.FILL;
+    topForm.setLayoutData(gData);
+    
+    
+    titlePanel = new Composite(topForm, SWT.NONE);
+    titlePanel.setLayout(new FillLayout());
+    gData = new GridData();
+    gData.grabExcessHorizontalSpace = true;
+    gData.grabExcessVerticalSpace = true;
+    gData.horizontalAlignment = SWT.FILL;
+    gData.verticalAlignment = SWT.FILL;
+    titlePanel.setLayoutData(gData);
+   
+    buttonPanel = new Composite(topForm, SWT.NONE);
+    buttonPanel.setLayout(new FillLayout());
+    gData = new GridData();
+    gData.grabExcessHorizontalSpace = false;
+    buttonPanel.setLayoutData(gData);
+    
     body = new Composite(mainComposite, SWT.None);
     
     data = new GridData();
@@ -107,32 +147,28 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
     for(XulComponent comp : getChildNodes()){
       if(comp instanceof XulToolbar){
         toolbar = (XulToolbar) comp;
-        ((Control) comp.getManagedObject()).setParent(topForm);
-        topForm.setTopCenter((Control) comp.getManagedObject());
+        ((Control) comp.getManagedObject()).setParent(toolbarPanel);
         
       } else if(comp instanceof XulCaption){
         if(lbl == null){
-          lbl = new CLabel(topForm, SWT.None);
+          lbl = new CLabel(titlePanel, SWT.None);
           Color[] inactiveGradient = new Color[]{Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT),
               Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND)
               , null};
-          int[] gradPercent = new int[] {25,100};            
+          int[] gradPercent = new int[] {25,100};
           //lbl.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_FOREGROUND));
           lbl.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_BACKGROUND));
 
           lbl.setBackground(inactiveGradient, gradPercent);
           lbl.addPaintListener(new PaintListener(){
-
             public void paintControl(PaintEvent arg0) {
               GC gc = arg0.gc;
               gc.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
               gc.drawLine(0, topForm.getBounds().height-1, topForm.getBounds().width, topForm.getBounds().height-1);
             }
-            
           });
         }
         lbl.setText(((XulCaption) comp).getLabel());
-        topForm.setTopLeft(lbl);
         
       } else {
         ((Control) comp.getManagedObject()).setParent(body);
@@ -140,13 +176,21 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
       
     }
     if(isCollapsible() && btn == null){
-      final ToolBar bar = new ToolBar(topForm, SWT.FLAT | SWT.HORIZONTAL);
+      final ToolBar bar = new ToolBar(buttonPanel, SWT.FLAT | SWT.HORIZONTAL);
       btn = new ToolItem(bar, SWT.PUSH);
-      btn.setText("_");
+      
+      InputStream in = SwtEditpanel.class.getClassLoader().getResourceAsStream("org/pentaho/ui/xul/swt/tags/images/16x16_right.png");
+      this.rightImg = new Image(bar.getDisplay(), in);
+      in = SwtEditpanel.class.getClassLoader().getResourceAsStream("org/pentaho/ui/xul/swt/tags/images/16x16_left.png");
+      this.leftImg = new Image(bar.getDisplay(), in);
+      
+      btn.setImage(rightImg);
+    
+      
       btn.addSelectionListener(new SelectionListener(){
         public void widgetDefaultSelected(SelectionEvent se) {}
         public void widgetSelected(SelectionEvent se) {
-          collapse();
+          collapse( !collapsed);
         }
       });
       bar.addPaintListener(new PaintListener(){
@@ -159,33 +203,35 @@ public class SwtEditpanel extends AbstractSwtXulContainer implements XulEditpane
         
       });
  
-      topForm.setTopRight(bar);
     }
     setManagedObject(body);
     super.layout();
     setManagedObject(mainComposite);
   }
   
-  public void collapse(){
-    body.setVisible(collapsed);
-    //((GridData) body.getLayoutData()).exclude = !collapsed;
+  public boolean isCollapsed(){
+    return collapsed;
+  }
+  public void collapse(boolean collapse){
+    this.collapsed = collapse;
+    body.setVisible(!collapsed);
+    ((GridData) body.getLayoutData()).exclude = collapsed;
     if(lbl != null){
-      if(! collapsed){
-        cachedLabel = lbl.getText();
-        lbl.setText(lbl.getText().substring(0,6)+"...");
-      } else {
-        lbl.setText(cachedLabel);
-      }
-    }
-    if(toolbar != null){  
-      toolbar.setVisible(collapsed);
-    }
-    collapsed = !collapsed;
+      titlePanel.setVisible(!collapsed);
+      ((GridData) titlePanel.getLayoutData()).exclude = collapsed;
+    }  
+    //toolbarPanel.setVisible(!collapsed);
+    
     if(collapsed){
+      btn.setImage(leftImg);
       hiddenflex = getFlex();
+      hiddenWidth = getWidth();
       setFlex(0);
+      setWidth(0);
     } else {
+      btn.setImage(rightImg);
       setFlex(hiddenflex);
+      setWidth(hiddenWidth);
     }
     ((SwtElement) getParent()).layout();
   }
