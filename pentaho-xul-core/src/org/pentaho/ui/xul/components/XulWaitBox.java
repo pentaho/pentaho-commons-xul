d926e53f102f09cce200904432fb8f4aea054995
Nick Baker <nbaker@pentaho.com>
2010-03-11 18:38:47 +0000

    [AGILEBI-259] New interface for wait dialogs

---
+++ pentaho-xul-core/src/org/pentaho/ui/xul/components/XulWaitBox.java
@@ -0,0 +1,16 @@
+package org.pentaho.ui.xul.components;
+
+
+public interface XulWaitBox  extends XulProgressmeter {
+  
+  public void setRunnable(WaitBoxRunnable runnable);
+  
+  public void start();
+  public void stop();
+  public void setModalParent(Object parent);
+  public void setCanCancel(boolean canCancel);
+  public void setTitle(String title);
+  public String getTitle();
+  public void setMessage(String message);
+  public String getMessage();
+}
