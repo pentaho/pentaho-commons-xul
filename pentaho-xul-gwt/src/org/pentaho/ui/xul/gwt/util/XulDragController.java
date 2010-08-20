8f22acd9cfa88e74245f8327440486abd04e3ab7
Nick Baker <nbaker@pentaho.com>
2010-08-20 01:54:15 +0000

    [BISERVER-4906] Using AbsolutePanel as DnD boundary panel

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/util/XulDragController.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/util/XulDragController.java
@@ -3,9 +3,13 @@ package org.pentaho.ui.xul.gwt.util;
 import com.allen_sauer.gwt.dnd.client.DragContext;
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.allen_sauer.gwt.dnd.client.VetoDragException;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+import org.pentaho.gwt.widgets.client.dialogs.GlassPane;
+import org.pentaho.gwt.widgets.client.dialogs.GlassPaneListener;
 import org.pentaho.gwt.widgets.client.ui.Draggable;
 
 /**
@@ -15,7 +19,7 @@ import org.pentaho.gwt.widgets.client.ui.Draggable;
 public class XulDragController extends PickupDragController {
 
   private Widget proxy;
-  private static XulDragController instance = new XulDragController();
+  private static XulDragController instance;
 
   public Widget getProxy() {
     return proxy;
@@ -25,13 +29,33 @@ public class XulDragController extends PickupDragController {
     this.proxy = proxy;
   }
 
-  private XulDragController() {
-    super(RootPanel.get(), false);
+  private XulDragController(AbsolutePanel panel) {
+    super(panel, false);
     setBehaviorDragProxy(true);
     setBehaviorDragStartSensitivity(5);
   }
 
   public static XulDragController getInstance(){
+    if(instance == null){
+      final AbsolutePanel panel = new AbsolutePanel();
+      panel.setHeight("100%");
+      panel.setWidth("100%");
+      panel.getElement().getStyle().setProperty("position", "absolute");
+      panel.getElement().getStyle().setProperty("top", "0px");
+      panel.getElement().getStyle().setProperty("left", "0px");
+      RootPanel.get().add(panel);
+      instance = new XulDragController(panel);
+      GlassPane.getInstance().addGlassPaneListener(new GlassPaneListener(){
+        public void glassPaneShown() throws Exception {
+          panel.setVisible(true);
+        }
+
+        public void glassPaneHidden() throws Exception {
+          panel.setVisible(false);
+        }
+      });
+
+    }
     return instance;
   }
 
