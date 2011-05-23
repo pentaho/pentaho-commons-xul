599eee167c87444c9f15577060b5a58af2e4f726
Ezequiel Cuellar <ecuellar@pentaho.com>
2011-05-23 18:01:56 +0000

    Fix for BISERVER-6035 UI Update - Data Source Wizard - Preview on CSV step needs new scrollbars

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtLabel.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtLabel.java
@@ -142,7 +142,8 @@ public class GwtLabel extends AbstractGwtXulComponent implements XulLabel {
           style += "height:" + getHeight() + "px;";
         }
       style += "\"";
-      label.getElement().setInnerHTML("<pre class='xul-pre' " + style +">" + value + "</pre>");
+      label.getElement().setInnerHTML("<div class='scroll-panel'> <pre class='xul-pre' " + style +">" + value + "</pre></div>");
+      replaceScrollbars();
     }
   }
   
