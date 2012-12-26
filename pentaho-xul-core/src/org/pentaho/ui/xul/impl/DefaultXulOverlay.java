9dd867cc40d8ace86b003c3c84c2da7b77f77132
Peter Minutillo <pminutillo@pentaho.com>
2012-12-26 17:17:40 +0000

    PUC Toolbar layout is wrong. Adding support for overlay priority attribute. Added support for priority attribute to PluginManager REST calls for overlays and perspectives. Added priority to plugin.xml

--- pentaho-xul-core/src/org/pentaho/ui/xul/impl/DefaultXulOverlay.java
+++ pentaho-xul-core/src/org/pentaho/ui/xul/impl/DefaultXulOverlay.java
@@ -1,3 +1,20 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ *   terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ *   Foundation.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License along with this
+ *   program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *   or from the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   Copyright 2008 - 2012 Pentaho Corporation.  All rights reserved.
+ */
+
 package org.pentaho.ui.xul.impl;
 
 import java.util.List;
@@ -11,13 +28,19 @@ public class DefaultXulOverlay implements XulOverlay {
   private String overlayUri;
   private String source;
   private String resourceBundleUri;
+  private int priority;
   private List<XulOverlayFragment> fragments;
-  
+
   public DefaultXulOverlay( String id, String overlayUri, String source, String resourceBundleUri ) {
+    this(id,overlayUri,source,resourceBundleUri,DEFAULT_PRIORITY);
+  }
+
+  public DefaultXulOverlay( String id, String overlayUri, String source, String resourceBundleUri, int priority ) {
     this.id = id;
     this.overlayUri = overlayUri;
     this.source = source;
     this.resourceBundleUri = resourceBundleUri;
+    this.priority = priority;
   }
   
   public DefaultXulOverlay(String overlayUri){
@@ -47,6 +70,9 @@ public class DefaultXulOverlay implements XulOverlay {
   public List<XulOverlayFragment> getOverlayFragments() {
     return fragments;
   }
-  
+
+  public int getPriority(){
+      return priority;
+  }
   
 }
--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtOverlay.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/tags/GwtOverlay.java
@@ -1,3 +1,20 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ *   terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ *   Foundation.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License along with this
+ *   program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *   or from the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   Copyright 2008 - 2012 Pentaho Corporation.  All rights reserved.
+ */
+
 package org.pentaho.ui.xul.gwt.tags;
 
 import org.pentaho.ui.xul.XulComponent;
@@ -68,7 +85,10 @@ public class GwtOverlay extends AbstractGwtXulContainer implements XulOverlay{
     return null;
   }
 
-  
+  public int getPriority() {
+      // TODO Auto-generated method stub
+      return 9999;
+  }
   
 }
 
