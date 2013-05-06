47ca36bc3a3f486c6f76a94aeaf04f5cd496d73a
jthomas <jthomas@penthao.com>
2013-05-06 15:13:18 -0400

    BISERVER-8895 - Manage Data Sources Window has formatting issues due to HTML5 change

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/util/GenericDialog.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/util/GenericDialog.java
@@ -53,7 +53,12 @@ public abstract class GenericDialog extends AbstractGwtXulContainer{
       dialog.hide();
     }
   }
-  
+
+  private native void logWarning(String log)
+  /*-{
+      $wnd.console.warn(log);
+  }-*/;
+
   public void show(){
     // Instantiation if delayed to prevent errors with the underlying GWT's not being able to calculate available size
     // in the case that the GWT app has been loaded into an iframe that's not visible.
@@ -102,6 +107,12 @@ public abstract class GenericDialog extends AbstractGwtXulContainer{
  
     if(getWidth() > 0){
       contents.setWidth(getWidth() + "px");//$NON-NLS-1$
+    }
+    logWarning("Height is here!!!!!!!!!!!!!!!!!");
+    if(getHeight() > 0){
+      int offsetHeight = getHeight() - HEADER_HEIGHT;
+      logWarning("Set height to: "+offsetHeight);
+      contents.setHeight(offsetHeight + "px");//$NON-NLS-1$
     }
     dialog.center();
     dialog.show();
