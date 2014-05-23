9d174503dc9874fffe99396a51e8d9780a443fa1
jeffreythomas <jthomas@pentaho.com>
2014-05-23 15:22:47 -0400

    [BISERVER-10746] - Plugins can't create submenus and share them

--- pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/overlay/AddElementOverlayAction.java
+++ pentaho-xul-gwt/src/org/pentaho/ui/xul/gwt/overlay/AddElementOverlayAction.java
@@ -17,6 +17,7 @@
 
 package org.pentaho.ui.xul.gwt.overlay;
 
+import org.pentaho.ui.xul.XulComponent;
 import org.pentaho.ui.xul.dom.Element;
 import org.pentaho.ui.xul.gwt.AbstractGwtXulComponent;
 
@@ -97,24 +98,42 @@ public class AddElementOverlayAction implements IOverlayAction {
   }
 
   public void perform() {
-    switch ( insertType ) {
-      case BEFORE:
-        parent.addChildAt( element, parent.getChildNodes().indexOf( relativeElement ) );
-        break;
-      case AFTER:
-        parent.addChildAt( element, parent.getChildNodes().indexOf( relativeElement ) + 1 );
-        break;
-      case FIRST:
-        parent.addChildAt( element, 0 );
-        break;
-      case POSITION:
-        parent.addChildAt( element, position );
-        break;
-      case LAST:
-      default:
-        parent.addChild( element );
-        break;
+
+    int size = parent.getChildNodes().size();
+    boolean existingSubMenu = false;
+    for ( int i = 0; i < size; i++ ) {
+      XulComponent comp = parent.getChildNodes().get( i );
+      if ( comp.getId() == element.getAttributeValue( "id" ) ) {
+        existingSubMenu = true;
+        for ( XulComponent node : element.getChildNodes() ) {
+          Element nodeElement = node;
+          comp.addChild( nodeElement );
+        }
+        ( (AbstractGwtXulComponent) comp ).layout();
+      }
     }
+
+    if ( !existingSubMenu ) {
+      switch ( insertType ) {
+        case BEFORE:
+          parent.addChildAt( element, parent.getChildNodes().indexOf( relativeElement ) );
+          break;
+        case AFTER:
+          parent.addChildAt( element, parent.getChildNodes().indexOf( relativeElement ) + 1 );
+          break;
+        case FIRST:
+          parent.addChildAt( element, 0 );
+          break;
+        case POSITION:
+          parent.addChildAt( element, position );
+          break;
+        case LAST:
+        default:
+          parent.addChild( element );
+          break;
+      }
+    }
+
     ( (AbstractGwtXulComponent) parent ).layout();
   }
 
