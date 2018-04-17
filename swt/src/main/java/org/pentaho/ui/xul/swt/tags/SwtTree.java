0261b9df84bb233129edee5ee24365cd5233dcc4
Ben Morrise <bmorrise@pentaho.com>
2018-04-17 09:56:44 -0400

    [BACKLOG-22287] Lazy loading of folders in Repository Explorer (#147)

--- swt/src/main/java/org/pentaho/ui/xul/swt/tags/SwtTree.java
+++ swt/src/main/java/org/pentaho/ui/xul/swt/tags/SwtTree.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
+ * Copyright (c) 2002-2018 Hitachi Vantara..  All rights reserved.
  */
 
 package org.pentaho.ui.xul.swt.tags;
@@ -179,6 +179,8 @@ public class SwtTree extends AbstractSwtXulContainer implements XulTree {
   private boolean hiddenRoot = true;
 
   private String command;
+
+  private String toggle;
 
   private boolean preserveExpandedState;
 
@@ -359,6 +361,11 @@ public class SwtTree extends AbstractSwtXulContainer implements XulTree {
         if ( arg0.getElement() instanceof XulTreeItem ) {
           XulTreeItem t = (XulTreeItem) arg0.getElement();
           t.setExpanded( false );
+          if ( toggle != null ) {
+            SearchBundle b = findSelectedIndex( new SearchBundle(), getRootChildren(), t );
+            Object[] toggled = new Object[] { b.selectedItem };
+            invoke( toggle, new Object[] { toggled, false } );
+          }
         }
       }
 
@@ -366,6 +373,11 @@ public class SwtTree extends AbstractSwtXulContainer implements XulTree {
         if ( arg0.getElement() instanceof XulTreeItem ) {
           XulTreeItem t = (XulTreeItem) arg0.getElement();
           t.setExpanded( true );
+          if ( toggle != null ) {
+            SearchBundle b = findSelectedIndex( new SearchBundle(), getRootChildren(), t );
+            Object[] toggled = new Object[] { b.selectedItem };
+            invoke( toggle, new Object[] { toggled, true } );
+          }
         }
       }
     } );
@@ -2243,6 +2255,14 @@ public class SwtTree extends AbstractSwtXulContainer implements XulTree {
 
   public void setCommand( String command ) {
     this.command = command;
+  }
+
+  public String getToggle() {
+    return toggle;
+  }
+
+  public void setToggle( String toggle ) {
+    this.toggle = toggle;
   }
 
   public boolean isPreserveexpandedstate() {
