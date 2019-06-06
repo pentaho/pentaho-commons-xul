98a330b1021aeceabfd52f66b57176e1603c3659
Kurtis Walker <kurtis.walker@hitachivantara.com>
2019-06-06 13:41:51 -0400

    [BACKLOG-27345] - setSize is wrong way to size a label.  using FormData instead

--- swt/src/main/java/org/pentaho/ui/xul/swt/tags/SwtSpacer.java
+++ swt/src/main/java/org/pentaho/ui/xul/swt/tags/SwtSpacer.java
@@ -12,16 +12,15 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
- */
-
-/**
- * 
+ * Copyright (c) 2002-2019 Hitachi Vantara..  All rights reserved.
  */
 
 package org.pentaho.ui.xul.swt.tags;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.pentaho.ui.xul.XulComponent;
@@ -40,25 +39,17 @@ public class SwtSpacer extends SwtElement implements XulSpacer {
   public SwtSpacer( Element self, XulComponent parent, XulDomContainer domContainer, String tagName ) {
     super( "spacer" );
     strut = new Label( (Composite) parent.getManagedObject(), SWT.NONE );
-    strut.setSize( 5, 5 );
+
+    String attrHeight = self.getAttributeValue( "height" );
+    int height = StringUtils.isEmpty( attrHeight ) ? 5 : Integer.parseInt( attrHeight );
+    String attrWidth = self.getAttributeValue( "width" );
+    int width = StringUtils.isEmpty( attrWidth ) ? 5 : Integer.parseInt( attrWidth );
+    FormData fdSpacer = new FormData();
+    fdSpacer.left = new FormAttachment( 0, 0 );
+    fdSpacer.right = new FormAttachment( 0, width );
+    fdSpacer.top = new FormAttachment( 0, 0 );
+    fdSpacer.bottom = new FormAttachment( 0, height );
+    strut.setLayoutData( fdSpacer );
     setManagedObject( strut );
-  }
-
-  public void setWidth( int size ) {
-    int y = strut.getSize().y;
-    strut.setSize( size, y );
-  }
-
-  public void setHeight( int size ) {
-    int x = strut.getSize().x;
-    strut.setSize( x, size );
-  }
-
-  @Override
-  public void setFlex( int flex ) {
-    super.setFlex( flex );
-  }
-
-  public void layout() {
   }
 }
