b72234ced74dc9dc031ea901db8a018030a4009a
Nicholas Jordan <nicholas.jordan@hitachivantara.com>
2023-03-10 14:28:24 -0500

    [BACKLOG-37098] xul tag <image> :: Functional images don't provide text alternative - GWT based Images - impl for GwtImage.java - refactor GwtToolbarbutton.java

--- gwt/impl/src/test/java/org/pentaho/ui/xul/gwt/tags/GwtImageTest.java
+++ gwt/impl/src/test/java/org/pentaho/ui/xul/gwt/tags/GwtImageTest.java
@@ -17,12 +17,12 @@
 
 package org.pentaho.ui.xul.gwt.tags;
 
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.xml.client.Element;
 import com.google.gwtmockito.GwtMockitoTestRunner;
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.pentaho.gwt.widgets.client.toolbar.ToolbarButton;
 import org.pentaho.ui.xul.gwt.tags.util.ImageUtil;
 
 import static org.mockito.Matchers.eq;
@@ -32,7 +32,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith( GwtMockitoTestRunner.class )
-public class GwtToolbarbuttonTest extends TestCase {
+public class GwtImageTest extends TestCase {
 
   @Test
   public void testSetAttribute_IMAGEALTTEXT() {
@@ -41,24 +41,24 @@ public class GwtToolbarbuttonTest extends TestCase {
     String altText1 = "SOME ALT TEST TEXT 1";
     String altText2 = "another alternative test text 2";
 
-    ToolbarButton mockToolbarButton = mock( ToolbarButton.class );
-    GwtToolbarbutton testInstance = new GwtToolbarbutton( mockToolbarButton );
+    Image mockImage = mock( Image.class );
+    GwtImage testInstance = new GwtImage( mockImage, null );
 
     // EXECUTE
     testInstance.setAttribute( attributeKey, altText1 );
     testInstance.setAttribute( "pen:" + attributeKey, altText2 ); // testing removal of "pen:"
 
     // VERIFY
-    verify( mockToolbarButton, times( 1 ) ).setImageAltText( eq( altText1 ) );
-    verify( mockToolbarButton, times( 1 ) ).setImageAltText( eq( altText2 ) );
+    verify( mockImage, times( 1 ) ).setAltText( eq( altText1 ) );
+    verify( mockImage, times( 1 ) ).setAltText( eq( altText2 ) );
   }
 
   @Test
   public void testGetImageAltText() {
-    // Scenario 1: null toolbarbutton
+    // Scenario 1: null image
 
     // SETUP
-    GwtToolbarbutton testInstance1 = new GwtToolbarbutton( null );
+    GwtImage testInstance1 = new GwtImage( null, null );
 
     // EXECUTE & VERIFY
     assertNull( testInstance1.getImageAltText() );
@@ -67,9 +67,9 @@ public class GwtToolbarbuttonTest extends TestCase {
 
     // SETUP
     String altText = "SOME TEST ALTERNATIVE IMAGE TEXT";
-    ToolbarButton mockToolbarButton = mock( ToolbarButton.class );
-    when( mockToolbarButton.getImageAltText() ).thenReturn( altText );
-    GwtToolbarbutton testInstance2 = new GwtToolbarbutton( mockToolbarButton );
+    Image mockImage = mock( Image.class );
+    when( mockImage.getAltText() ).thenReturn( altText );
+    GwtImage testInstance2 = new GwtImage( mockImage, null );
 
     // EXECUTE
     assertEquals( altText, testInstance2.getImageAltText() );
@@ -77,44 +77,43 @@ public class GwtToolbarbuttonTest extends TestCase {
 
   @Test
   public void testSetImageAltText() {
-    // Scenario 1: null toolbarbutton
+    // Scenario 1: null image
 
     // SETUP
-    GwtToolbarbutton testInstance1 = new GwtToolbarbutton( null );
+    GwtImage testInstance1 = new GwtImage( null, null );
 
     // EXECUTE & VERIFY
     // no exception is thrown here
     testInstance1.setImageAltText( "someText" );
 
-    // Scenario 2: non-null toolbarbutton
+    // Scenario 2: non-null image
 
     // SETUP
-    ToolbarButton mockToolbarButton = mock( ToolbarButton.class );
-    GwtToolbarbutton testInstance2 = new GwtToolbarbutton( mockToolbarButton );
+    Image mockImage = mock( Image.class );
+    GwtImage testInstance2 = new GwtImage( mockImage, null );
     String altText = "SOME TEST ALTERNATIVE IMAGE TEXT";
 
     // EXECUTE
     testInstance2.setImageAltText( altText );
 
     // VERIFY
-    verify( mockToolbarButton, times( 1 ) ).setImageAltText( eq( altText ) );
+    verify( mockImage, times( 1 ) ).setAltText( eq( altText ) );
   }
 
   @Test
   public void testInitSetImageAltText() {
     // SETUP
-    ToolbarButton mockToolbarButton = mock( ToolbarButton.class );
+    Image mockImage = mock( Image.class );
     ImageUtil mockImageUtil = mock( ImageUtil.class );
     Element mockElement = mock( Element.class );
     String altText = "test alternative image text";
-    when( mockImageUtil.getAltText( eq( mockElement ) ) )
-      .thenReturn( altText );
-    GwtToolbarbutton testInstance = new GwtToolbarbutton( "testToolbarbutton", mockToolbarButton, mockImageUtil );
+    when( mockImageUtil.getAltText( eq( mockElement ) ) ).thenReturn( altText );
+    GwtImage testInstance = new GwtImage( mockImage, mockImageUtil );
 
     // EXECUTE
     testInstance.initSetImageAltText( mockElement );
 
     // VERIFY
-    verify( mockToolbarButton, times( 1 ) ).setImageAltText( eq( altText ) );
+    verify( mockImage, times( 1 ) ).setAltText( eq( altText ) );
   }
 }
