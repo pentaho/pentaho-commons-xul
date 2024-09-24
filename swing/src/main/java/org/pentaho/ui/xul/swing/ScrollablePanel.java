7c35603e9d15a6076d2899db3fee04df7e4f6096
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:08:09 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 50b91735d704ff2edcac88511bf4c1a346199bab.

--- swing/src/main/java/org/pentaho/ui/xul/swing/ScrollablePanel.java
+++ swing/src/main/java/org/pentaho/ui/xul/swing/ScrollablePanel.java
@@ -1,14 +1,3 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
 /*jadclipse*/// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
 package org.pentaho.ui.xul.swing;
 
